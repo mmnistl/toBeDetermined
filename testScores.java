37b5b705930a1144958c54d1a1ebb80d5438b4a9
Amanda Morgan <106630710+amorg7208@users.noreply.github.com>
2023-05-30 14:49:20 -0700

    Update testScores
    Updated code so that the general majors grouping is deleted and only outputs specific majors to console based on average tilt score input by user.

--- testScores.java
+++ testScores.java
@@ -2,16 +2,11 @@ import java.util.Scanner;
 
 public class testScores {
     public static void main(String[] args) {
-        //Create one array for the upper range and one array for the lower range of each grouping of tilt scores.
         double[] lowerRanges = {-0.47, -0.30, -0.19, -0.18, -0.11, -0.04,
-                                -0.04, -0.01, 0.06, 0.12, 0.18, 0.27, 0.42};
+                -0.04, -0.01, 0.06, 0.12, 0.18, 0.27, 0.42};
         double[] upperRanges = {-0.29, -0.20, -0.19, -0.12, -0.05, -0.04,
-                                -0.02, 0.05, 0.11, 0.17, 0.26, 0.41, Double.POSITIVE_INFINITY};
-        //Create an array for major groupings
-        String[] majorGroupings = {"English", "Communication", "Psychology", "Social Sciences", "Education",
-                                    "Liberal Arts", "Health Professions", "Biology", "Business",
-                                    "Physical Science", "Computer Science", "Engineering", "Mathematics"};
-        //Create an array for specific majors within the major groupings
+                -0.02, 0.05, 0.11, 0.17, 0.26, 0.41, Double.POSITIVE_INFINITY};
+
         String[][] specificMajors = {
                 {"Classical Studies", "English", "Linguistics"},
                 {"Communication"},
@@ -32,65 +27,27 @@ public class testScores {
                 {"Aeronautics and Astronautics", "Earth and Space Sciences", "Mathematics"}
         };
 
-        //Prompt the user to input their average tilt score
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your average tilt score? ");
-        double averageTiltScore = scanner.nextDouble();
+        Scanner console = new Scanner(System.in);
+        System.out.print("What is your average Tilt Score? ");
+        double averageTiltScore = console.nextDouble();
 
-        //Find the index of the matching major grouping
-        //Take avg tilt score, lower range and upper range as parameters to find the index of the
-        //matching major grouping. Store the results in matchingIndex.
         int matchingIndex = findMatchingIndex(averageTiltScore, lowerRanges, upperRanges);
 
-        //if matchingIndex does not equal -1 that means a match has been found. our range of tilt scores
-        //is from -0.47 to 0.42.
         if (matchingIndex != -1) {
-            //Display the matching major grouping
-            //Retrieves the specific major grouping that corresponds to the average tilt score provided by the user.
-            String matchingMajorGrouping = majorGroupings[matchingIndex];
-            System.out.println("Major Grouping: " + matchingMajorGrouping);
-
-            //Prompt the user if they want to see the specific majors within their majorGrouping
-            System.out.print("Would you like to see the specific majors in this grouping? (Y/N): ");
-            String choice = scanner.next();
-
-            if (choice.equalsIgnoreCase("Y")) {
-                //Display the specific majors within the majorGrouping based on their tilt score.
-                String[] majors = specificMajors[matchingIndex];
-                System.out.println("Specific Majors:");
-                for (String major : majors) {
-                    System.out.println("- " + major);
-                }
+            System.out.println("These are the majors that correspond with your average Tilt Score: ");
+            for (String major : specificMajors[matchingIndex]) {
+                System.out.println("- " + major);
             }
         } else {
             System.out.println("No matching major grouping found.");
         }
     }
-
     public static int findMatchingIndex(double averageTiltScore, double[] lowerRanges, double[] upperRanges) {
-        for (int i = 0; i < lowerRanges.length; i++) { //loop through the lower ranges array, continues to loop
-            //as long as i is less than the lower ranges array length.
-            //check the tilt score against the lower ranges array
+        for (int i = 0; i < lowerRanges.length; i++) {
             if (averageTiltScore >= lowerRanges[i] && averageTiltScore <= upperRanges[i]) {
-                //this checks to see if the tilt score is within a range between lower and upper ranges arrays
                 return i;
-                //return the position of the array index to determine the corresponding major group
             }
         }
-        return -1; //if no match is found, return -1.
+        return -1;
     }
 }
-/*
-//testing notes:
-if user chooses "no" when asked if they want to see the specific majors in their grouping,
-no further programming is executed.
-Thinking of where we can integrate this chunk of code into the core programming-
-Do we want to use this point to move onto asking about jobs in the specific major grouping?
-If a user inputs a tilt score that is out of range, do we want to prompt them to enter a score that is
-within the specified range? (-0.47 to 0.42)
-if the user types "yes" or "no" when asked if they want to see the specific majors, nothing happens.
-They have to specifically type "y" or "n" - Do we want to change this?
-Do you agree with how the majors are divided up into majorGroupings?
-Do we want to have something populate that explains what the average Tilt Score means? - before this chunk
-of code is executed? Make it an option to use tilt score?
-*/
