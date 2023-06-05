800ec2b7b8efe76bb8d67eaa8657d95031a8c040
Amanda Morgan <106630710+amorg7208@users.noreply.github.com>
2023-06-04 17:38:48 -0700

    Update tiltScores.java

--- tiltScores.java
+++ tiltScores.java
@@ -1,7 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+public class Project {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner main = new Scanner(System.in);
+        //files path with information or just string with information
+        String majorFile = "../project/Majors.txt";
+        String jobsFile = "../project/Jobs.txt";
+        String salariesFile = "../project/JobsSalaries.txt";
+        System.out.println("This is an interactive program that prompts the user to choose majors from a four-year university, then choose corresponding job titles.\nThe user will input student loan information to determine if their average salaries will be able to cover their monthly loan payments.\nThis is applicable to universities in the United States only.\n");
 
-public class tiltScores {
-    public static void main(String[] args) {
         // Create tilt score range arrays, upper and lower.
         double[] lowerRanges = {-0.47, -0.30, -0.19, -0.18, -0.11, -0.04, -0.04, -0.01, 0.06, 0.12, 0.18, 0.27, 0.42};
         double[] upperRanges = {-0.29, -0.20, -0.19, -0.12, -0.05, -0.04, -0.02, 0.05, 0.11, 0.17, 0.26, 0.41, 0.42};
@@ -26,38 +33,41 @@ public class tiltScores {
                         "Industrial Engineering", "Mechanical Engineering"},
                 {"Aeronautics and Astronautics", "Earth and Space Sciences", "Mathematics"}
         };
-
         getUserTiltScore(lowerRanges, upperRanges, specificMajors);
-    }
-
+        
+    //tiltScore
     public static void getUserTiltScore(double[] lowerRanges, double[] upperRanges, String[][] specificMajors) {
         Scanner console = new Scanner(System.in);
-        String majorFile = "../project/Majors.txt";//This is not right.
 
-        System.out.print("Would you like to use your average Tilt Score to determine your major? (yes/no) ");
+        System.out.print("Would you like to use your Tilt Score to determine your major? (yes/no) ");
         String response = console.nextLine();
 
         if (response.equalsIgnoreCase("yes")) {
-            System.out.print("What is your average Tilt Score? ");
-            double averageTiltScore = console.nextDouble();
+            System.out.print("What is your Tilt Score? ");
 
-            int matchingIndex = findMatchingIndex(averageTiltScore, lowerRanges, upperRanges);
+            while (!console.hasNextDouble()) {
+                System.out.println("Invalid input. Please enter a number in decimal format.");
+                System.out.print("What is your Tilt Score? ");
+                console.nextLine();
+            }
+
+            double tiltScore = console.nextDouble();
+            console.nextLine();
+
+            int matchingIndex = findMatchingIndex(tiltScore, lowerRanges, upperRanges);
 
             if (matchingIndex >= 0 && matchingIndex < specificMajors.length) {
-                System.out.println("These are the majors that correspond to your average Tilt Score:");
+                System.out.println("These are the majors that correspond to your Tilt Score:");
                 for (String major : specificMajors[matchingIndex]) {
                     System.out.println("- " + major);
                 }
+                System.out.println();
+                System.out.println("Additionally, here is the complete list of Majors: \n");
             } else {
-                System.out.println("No matching majors found based on your average Tilt Score.");
+                System.out.println("No matching majors found based on your Tilt Score.\n");
             }
-        } else if (response.equalsIgnoreCase("no")) {
-            System.out.print("Would you like to see the complete list of majors? (yes/no) ");
-            response = console.nextLine();
-
-            if (response.equalsIgnoreCase("yes")) {
-                System.out.println(majorFile);
-            }
+        } else if(response.equalsIgnoreCase("no")){
+            System.out.print("Here is the complete list of Majors to choose from: \n");
         }
     }
 
@@ -69,4 +79,3 @@ public class tiltScores {
         }
         return -1;
     }
-}
